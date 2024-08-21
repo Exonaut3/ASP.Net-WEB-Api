65a69eed96d7e395ec30f08e9b3254fc8e0cb34a
Simeon Dvojakov <dvojakovsimeon@gmail.com>
2024-08-21 19:59:49 +0200

    Prvoto domashno

---
+++ HomeworkClass2/Homework.Database/Database.cs
@@ -0,0 +1,7 @@
+﻿namespace Homework.Database
+{
+    public static class Database
+    {
+        public static List<string> Usernames = new List<string>() { "Bobinator", "RyanGoslingFan420", "PlainJane1990", "XXX_SERIALCHILLER_XXX" };
+    }
+}
---
+++ HomeworkClass2/HomeworkClass2/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using Homework.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkClass2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Database.Usernames;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id <= 0 || id > Database.Usernames.Count())
+            {
+                return NotFound();
+            }
+            return Database.Usernames[id-1];
+        }
+
+        [HttpPost]
+        public ActionResult Post(string username)
+        {
+            if (String.IsNullOrEmpty(username))
+            {
+                return BadRequest();
+            }
+            Database.Usernames.Add(username);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            if (id <= 0 || id > Database.Usernames.Count())
+            {
+                return NotFound();
+            }
+            var removedUser = Database.Usernames[id-1];
+            Database.Usernames.Remove(removedUser);
+            return StatusCode(StatusCodes.Status200OK);
+        }
+
+    }
+}
